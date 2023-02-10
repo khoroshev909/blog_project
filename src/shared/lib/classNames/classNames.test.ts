88e588f393fecaa295f2eabd32694c384556934a
Eugene Khoroshev <khoroshev909@gmail.com>
2023-02-10 14:05:24 +0300

    add jest unit tests

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,22 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with one class', () => {
+        expect(classNames('someClass')).toBe('someClass');
+    });
+
+    test('with additional classes', () => {
+        const expected = classNames('someClass', {}, ['class1', 'class2']);
+        expect(expected).toBe('someClass class1 class2');
+    });
+
+    test('with mods', () => {
+        const expected = classNames('someClass', { selected: true }, ['class1', 'class2']);
+        expect(expected).toBe('someClass class1 class2 selected');
+    });
+
+    test('with mods undefined', () => {
+        const expected = classNames('someClass', { selected: true, active: undefined }, ['class1', 'class2']);
+        expect(expected).toBe('someClass class1 class2 selected');
+    });
+});
