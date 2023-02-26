562932fe9c0fb344583760cff89a254e35f740ba
Eugene Khoroshev <khoroshev909@gmail.com>
2023-02-26 10:12:01 +0500

    fixed modal dark theme, add json-server

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -15,6 +15,7 @@ export const useTheme = (): UseThemeResult => {
     const toggleTheme = () => {
         const newTheme = theme === THEMES.DARK ? THEMES.LIGHT : THEMES.DARK;
         setTheme(newTheme);
+        document.body.className = newTheme;
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
 
